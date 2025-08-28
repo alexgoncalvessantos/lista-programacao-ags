b35d683734d5f169afb6f0056a54086ae1e3e456
alexgoncalvessantos <alexgvssantos@gmail.com>
2025-08-28 19:45:35 -0300

    atividade1

--- AtividadesJava/src/Atividade1.java
+++ AtividadesJava/src/Atividade1.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
-
 public class Atividade1 {
+    public static void main(String[] args) {
+        String nome = "Alex"; String cidade = " Itabira"; final String PAIS = "Brasil";
+
+        System.out.println("Olá eu sou " + nome + " moro em " + cidade + "/" + PAIS);
+    }
 }
